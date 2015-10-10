08f308d0bba4bdc965c513a69251bc4d5cabb154
gincwang <gincwang@gmail.com>
2015-10-09 21:00:56 -0700

    finish all spec tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -64,6 +64,7 @@ $(function() {
          it('is hidden by default', function(){
              expect(hidden.length).toBe(1);
          });
+
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
@@ -81,24 +82,38 @@ $(function() {
      });
     /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function(){
+
+        beforeEach(function(done){
+            loadFeed(0,done);
+        });
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test wil require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-         it('has at least 1 entry', function(){
-
+         it('has at least 1 entry', function(done){
+             var feed = ('.feed');
+             expect(feed.length).toBeGreaterThan(1);
+             done();
          });
      });
     /* TODO: Write a new test suite named "New Feed Selection"*/
     describe('New Feed Selection', function(){
+        var oldContent;
+
+        beforeEach(function(done){
+            oldContent = $('.feed').html();
+            loadFeed(1,done);
+        });
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-         it('changes content when loaded', function(){
-
+         it('changes content when loaded', function(done){
+             var newContent = $('.feed').html();
+             expect(oldContent).not.toBe(newContent);
+             done();
          });
     });
 }());
